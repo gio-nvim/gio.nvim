8780bd3998c263ae2cfd4272a9d426de5bf423a4
shift-d <shiftkeydeveloper@gmail.com>
2022-07-22 08:30:09 +0300

    [vim]: set locale

--- lua/modules/vim/init.lua
+++ lua/modules/vim/init.lua
@@ -6,6 +6,7 @@ return {
             ["options"] = {},
             ["plugins"] = {},
             ["keymaps"] = {},
+            ["locale"] = {},
         }
     }
 }
---
+++ lua/modules/vim/locale.lua
@@ -0,0 +1,3 @@
+vim.cmd "runtime delmenu"
+vim.cmd "language en_US.utf8"
+vim.cmd "runtime menu"
