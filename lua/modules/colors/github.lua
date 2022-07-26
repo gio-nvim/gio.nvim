f09062e154be44ae59cfc98d88486a8ec16f6984
shift-d <shiftkeydeveloper@gmail.com>
2022-07-26 21:14:12 +0300

    [colors]: catppuccin -> github
    keeping catppuccin as a not enabled source of module

---
+++ lua/modules/colors/github.lua
@@ -0,0 +1,12 @@
+require "github-theme".setup {
+    colors = {},
+    comment_style = "italic",
+    dark_float = true,
+    dark_sidebar = true,
+    hide_end_of_buffer = false,
+    hide_inactive_statusline = false,
+    keyword_style = "NONE",
+    msg_area_style = "NONE",
+    sidebars = { "packer" },
+    theme_style = "dark_default",
+}
--- lua/modules/colors/init.lua
+++ lua/modules/colors/init.lua
@@ -3,7 +3,8 @@ return {
     ["description"] = "Module that manages highlights-related stuff",
     ["source"] = {
         ["packages"] = {
-            ["catppuccin/nvim"] = { as = "catppuccin", config = "theme" },
+            --["catppuccin/nvim"] = { as = "catppuccin", config = "catppuccin" },
+            ["projekt0n/github-nvim-theme"] = { config = "github" },
             ["nvim-treesitter/nvim-treesitter"] = {
                 config = "parsers",
                 event = "BufEnter"
